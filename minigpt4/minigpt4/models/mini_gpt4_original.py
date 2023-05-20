566d346f139f2f259937d1fe60665bdbe985e2d6
Alexiland <Alexiland@outlook.com>
2023-05-19 20:04:17 -0700

    debug config is none error
    debug config is none error

--- minigpt4/minigpt4/models/mini_gpt4_original.py
+++ minigpt4/minigpt4/models/mini_gpt4_original.py
@@ -10,10 +10,6 @@ from minigpt4.common.registry import registry
 from minigpt4.models.blip2 import Blip2Base, disabled_train
 from minigpt4.models.modeling_llama import LlamaForCausalLM
 from transformers import LlamaTokenizer
-sys.path.append('/home/h5guo/work/esm')
-import esm
-
-import time
 
 
 @registry.register_model("mini_gpt4")
@@ -34,7 +30,6 @@ class MiniGPT4(Blip2Base):
         drop_path_rate=0,
         use_grad_checkpoint=False,
         vit_precision="fp16",
-        freeze_protein_encoder=True,
         freeze_vit=True,
         freeze_qformer=True,
         num_query_token=32,
@@ -112,56 +107,62 @@ class MiniGPT4(Blip2Base):
         self.llama_proj = nn.Linear(
             self.Qformer.config.hidden_size, self.llama_model.config.hidden_size
         )
-        for name, param in self.llama_proj.named_parameters():
-            param.requires_grad = False
-
-        self.esm_llama_proj = nn.Linear(
-            512, self.llama_model.config.hidden_size
-        )
         self.max_txt_len = max_txt_len
         self.end_sym = end_sym
 
         if prompt_path:
             with open(prompt_path, 'r') as f:
                 raw_prompts = f.read().splitlines()
-            filted_prompts = [raw_prompt for raw_prompt in raw_prompts if "<proteinHere>" in raw_prompt]
+            filted_prompts = [raw_prompt for raw_prompt in raw_prompts if "<ImageHere>" in raw_prompt]
             self.prompt_list = [prompt_template.format(p) for p in filted_prompts]
             print('Load {} training prompts'.format(len(self.prompt_list)))
             print('Prompt Example \n{}'.format(random.choice(self.prompt_list)))
         else:
             self.prompt_list = []
 
-    def encode_protein(self, protein_encode):
-        device = protein_encode.device
-        # with self.maybe_autocast():
+    def vit_to_cpu(self):
+        self.ln_vision.to("cpu")
+        self.ln_vision.float()
+        self.visual_encoder.to("cpu")
+        self.visual_encoder.float()
 
-        # # image_embeds is of shape [B, 257, 1408]
-        # query_output.last_hidden_state is of shape [B, 32, 768]
-        # _, encoder_out = self.protein_encoder.sample(coords, temperature=1, device=torch.device('cuda')).to(device)
-        protein_embeds = protein_encode.to(device)
-        # protein_embeds = encoder_out["encoder_out"][0]
-        # print(f"protein_embeds.shape: {protein_embeds.shape}")
+    def encode_img(self, image):
+        device = image.device
+        if self.low_resource:
+            self.vit_to_cpu()
+            # image is of shape[B, 3, 224, 224]
+            image = image.to("cpu")
 
-        # input llama is of shape [B, 32, 5120]
-        inputs_llama = self.esm_llama_proj(protein_embeds.squeeze(dim=2))
-        # atts_llama is of shape [B, 32]
-        atts_llama = torch.ones(inputs_llama.size()[:-1], dtype=torch.long).to(device)
+        with self.maybe_autocast():
+            # image_embeds is of shape [B, 257, 1408]
+            image_embeds = self.ln_vision(self.visual_encoder(image)).to(device)
+            image_atts = torch.ones(image_embeds.size()[:-1], dtype=torch.long).to(device)
+
+            query_tokens = self.query_tokens.expand(image_embeds.shape[0], -1, -1)
+            query_output = self.Qformer.bert(
+                query_embeds=query_tokens,
+                encoder_hidden_states=image_embeds,
+                encoder_attention_mask=image_atts,
+                return_dict=True,
+            )
+            # query_output.last_hidden_state is of shape [B, 32, 768]
+
+            # input llama is of shape [B, 32, 5120]
+            inputs_llama = self.llama_proj(query_output.last_hidden_state)
+            # atts_llama is of shape [B, 32]
+            atts_llama = torch.ones(inputs_llama.size()[:-1], dtype=torch.long).to(image.device)
         return inputs_llama, atts_llama
 
     def prompt_wrap(self, img_embeds, atts_img, prompt):
         if prompt:
             batch_size = img_embeds.shape[0]
-            p_before, p_after = prompt.split('<proteinHere>')
+            p_before, p_after = prompt.split('<ImageHere>')
             p_before_tokens = self.llama_tokenizer(
                 p_before, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
             p_after_tokens = self.llama_tokenizer(
                 p_after, return_tensors="pt", add_special_tokens=False).to(img_embeds.device)
             p_before_embeds = self.llama_model.model.embed_tokens(p_before_tokens.input_ids).expand(batch_size, -1, -1)
             p_after_embeds = self.llama_model.model.embed_tokens(p_after_tokens.input_ids).expand(batch_size, -1, -1)
-            # print("concatenating embeddings")
-            # print(p_before_embeds.shape)
-            # print(img_embeds.shape)
-            # print(p_after_embeds.shape)
             wrapped_img_embeds = torch.cat([p_before_embeds, img_embeds, p_after_embeds], dim=1)
             wrapped_atts_img = atts_img[:, :1].expand(-1, wrapped_img_embeds.shape[1])
             return wrapped_img_embeds, wrapped_atts_img
@@ -169,22 +170,15 @@ class MiniGPT4(Blip2Base):
             return img_embeds, atts_img
 
     def forward(self, samples):
-        # print("[mini_gpt4] Forward Start-----------------------------")
-        start = time.time()
-        protein_encode = samples["encoder_out"]
-        start_encode_protein = time.time()
-        img_embeds, atts_img = self.encode_protein(protein_encode)
-        end_encode_protein = time.time()
-        # print(f"[mini_gpt4]Elapsed time encode_protein: {end_encode_protein - start_encode_protein}")
+        image = samples["image"]
+        img_embeds, atts_img = self.encode_img(image)
         if hasattr(samples, 'question_split'):  # VQA dataset
             print('VQA Batch')
-            vqa_prompt = '###Human: <protein><proteinHere></protein> '
+            vqa_prompt = '###Human: <Img><ImageHere></Img> '
             img_embeds, atts_img = self.prompt_wrap(img_embeds, atts_img, vqa_prompt)
         elif self.prompt_list:
             prompt = random.choice(self.prompt_list)
             img_embeds, atts_img = self.prompt_wrap(img_embeds, atts_img, prompt)
-        end_prompt_wrap = time.time()
-        # print(f"[mini_gpt4]Elapsed time prompt_wrap: {end_prompt_wrap - end_encode_protein}")
 
         self.llama_tokenizer.padding_side = "right"
 
@@ -197,21 +191,15 @@ class MiniGPT4(Blip2Base):
             truncation=True,
             max_length=self.max_txt_len,
             add_special_tokens=False
-        ).to(protein_encode.device)
-
-        end_to_regress_tokens = time.time()
-        # print(f"[mini_gpt4]Elapsed time to_regress_tokens: {end_to_regress_tokens - end_prompt_wrap}")
+        ).to(image.device)
 
         targets = to_regress_tokens.input_ids.masked_fill(
             to_regress_tokens.input_ids == self.llama_tokenizer.pad_token_id, -100
         )
 
-        end_targets = time.time()
-        # print(f"[mini_gpt4]Elapsed time targets: {end_targets - end_to_regress_tokens}")
-
         empty_targets = (
             torch.ones([atts_img.shape[0], atts_img.shape[1]+1],
-                       dtype=torch.long).to(protein_encode.device).fill_(-100)  # plus one for bos
+                       dtype=torch.long).to(image.device).fill_(-100)  # plus one for bos
         )
         targets = torch.cat([empty_targets, targets], dim=1)
 
@@ -219,17 +207,10 @@ class MiniGPT4(Blip2Base):
         bos = torch.ones([batch_size, 1],
                          dtype=to_regress_tokens.input_ids.dtype,
                          device=to_regress_tokens.input_ids.device) * self.llama_tokenizer.bos_token_id
-
         bos_embeds = self.llama_model.model.embed_tokens(bos)
-        end_bos_embeds = time.time()
-        # print(f"[mini_gpt4]Elapsed time bos_embeds: {end_bos_embeds - end_targets}")
         atts_bos = atts_img[:, :1]
 
         to_regress_embeds = self.llama_model.model.embed_tokens(to_regress_tokens.input_ids)
-
-        end_to_regress_embeds = time.time()
-        # print(f"[mini_gpt4]Elapsed time to_regress_embeds: {end_to_regress_embeds - end_bos_embeds}")
-
         inputs_embeds = torch.cat([bos_embeds, img_embeds, to_regress_embeds], dim=1)
         attention_mask = torch.cat([atts_bos, atts_img, to_regress_tokens.attention_mask], dim=1)
 
@@ -240,39 +221,30 @@ class MiniGPT4(Blip2Base):
                 return_dict=True,
                 labels=targets,
             )
-        end_outputs = time.time()
-        # print(f"[mini_gpt4]Elapsed time outputs: {end_outputs - end_to_regress_embeds}")
         loss = outputs.loss
-        # print(f"[mini_gpt4] Forward End-----------------------------{time.time() - start}")
+
         return {"loss": loss}
 
     @classmethod
     def from_config(cls, cfg):
-
         vit_model = cfg.get("vit_model", "eva_clip_g")
         q_former_model = cfg.get("q_former_model", "https://storage.googleapis.com/sfr-vision-language-research/LAVIS/models/BLIP2/blip2_pretrained_flant5xxl.pth")
         img_size = cfg.get("image_size")
         num_query_token = cfg.get("num_query_token")
         llama_model = cfg.get("llama_model")
 
-
-
         drop_path_rate = cfg.get("drop_path_rate", 0)
         use_grad_checkpoint = cfg.get("use_grad_checkpoint", False)
         vit_precision = cfg.get("vit_precision", "fp16")
-        freeze_protein_encoder = cfg.get("freeze_protein_encoder", True)
+        freeze_vit = cfg.get("freeze_vit", True)
         freeze_qformer = cfg.get("freeze_qformer", True)
         low_resource = cfg.get("low_resource", False)
         device_8bit = cfg.get("device_8bit", 0)
-
-
 
         prompt_path = cfg.get("prompt_path", "")
         prompt_template = cfg.get("prompt_template", "")
         max_txt_len = cfg.get("max_txt_len", 32)
         end_sym = cfg.get("end_sym", '\n')
-
-
 
         model = cls(
             vit_model=vit_model,
@@ -281,8 +253,7 @@ class MiniGPT4(Blip2Base):
             drop_path_rate=drop_path_rate,
             use_grad_checkpoint=use_grad_checkpoint,
             vit_precision=vit_precision,
-            freeze_protein_encoder=freeze_protein_encoder,
-            freeze_vit=True,
+            freeze_vit=freeze_vit,
             freeze_qformer=freeze_qformer,
             num_query_token=num_query_token,
             llama_model=llama_model,
@@ -293,7 +264,6 @@ class MiniGPT4(Blip2Base):
             low_resource=low_resource,
             device_8bit=device_8bit,
         )
-
 
         ckpt_path = cfg.get("ckpt", "")  # load weights of MiniGPT-4
         if ckpt_path:
